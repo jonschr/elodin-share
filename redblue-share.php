e0c00f00fd6672adc6ecc6175b7867111120c1c7
redblue <jon@redblue.us>
2017-10-23 12:06:23 -0500

    Initial commit

---
+++ redblue-share.php
@@ -0,0 +1,29 @@
+<?php
+/*
+	Plugin Name: Red Blue Sharing
+	Plugin URI: http://redblue.us
+	Description: An addon which sets up several ways to share
+	Version: 0.9
+    Author: Jon Schroeder
+    Author URI: http://redblue.us
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+*/
+
+/* Prevent direct access to the plugin */
+if ( !defined( 'ABSPATH' ) ) {
+    die( "Sorry, you are not allowed to access this page directly." );
+}
+
+// Plugin directory
+define( 'REDBLUE_SHARING', dirname( __FILE__ ) );
+
+add_shortcode( $tag, $func );
